fd9ef28f66c19aa954e91be17fc12590438d8944
Luis Fereira <luis@spartandevs.com>
2023-09-02 01:41:18 -0400

    [LFereira] add footer and styles

--- components/home/Home.jsx
+++ components/home/Home.jsx
@@ -17,10 +17,25 @@ export const Home = () => {
           </ul>
         </nav>
       </div>
-      <div className="container-general">
+      <div className="container">
         <div className="title-container">
-          <h2>Front-end Developer</h2>
-          <h4>Pagina de inicio</h4>
+          <h2>titulo</h2>
+        </div>
+        <h4>imagen</h4>
+      </div>
+
+      <div className="footer">
+        <div className="information">
+          <div className="copyrigth">
+            <p>copyrigth</p>
+          </div>
+          <div className="info-personal">
+            <p>projects</p>
+            <p>contact</p>
+            <p>(link linkedIn)</p>
+            <p>(link X)</p>
+            <p>(link facebook)</p>
+          </div>
         </div>
       </div>
     </>
